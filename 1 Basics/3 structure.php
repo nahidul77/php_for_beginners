8130a325f33554f743f0c42e084f4163369d5bf3
nahidul77 <nahidul.mee@gmail.com>
2022-04-12 22:03:29 +0600

    Basics structure & echo, print

---
+++ 1 Basics/3 structure.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>PHP</title>
+</head>
+<body>
+    <?php echo "Hello Everyone"; ?>
+</body>
+</html>
---
+++ 1 Basics/4 echo_print.php
@@ -0,0 +1,19 @@
+<?php
+    echo "Nahidul Islam \n";
+
+   echo 'Nahidul Islam'."\n";
+
+   echo ('Nahidul Islam')."\n";
+
+    echo "Nahidul","Islam \n";
+
+    echo "Nahidul"."Islam \n";
+
+    echo "<h1><i>Nahidul"."Islam</i></h1>";
+
+    echo 23.58;
+
+    print "<h1><i>Nahidul"."Islam</i></h1>";
+
+    print 23.58;
+?>
