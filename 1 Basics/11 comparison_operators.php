1727079135ff8200d18e59956af9d69f465ad8cb
nahidul77 <nahidul.mee@gmail.com>
2022-04-13 00:37:13 +0600

    Basics constant & all types of operators

---
+++ 1 Basics/11 comparison_operators.php
@@ -0,0 +1,17 @@
+<?php
+    $a = 10;
+    $b = 10;
+    /**
+     * comparison result would be two, true or false, especially it returns "1" or " "empty string
+     */
+    echo  $a == $b;
+    echo  $a === $b;
+    echo  $a != $b;
+    echo  $a <> $b;
+    echo  $a !== $b;
+    echo  $a > $b;
+    echo  $a < $b;
+    echo  $a >= $b;
+    echo  $a <= $b;
+    echo  $a <=> $b; //Spaceship (-1,0,1)
+?>
---
+++ 1 Basics/8 constant.php
@@ -0,0 +1,10 @@
+<?php
+    define("test",50);
+    echo test;
+
+    define("test1",50,true);
+    echo TEST1;
+
+   $sum = test + 20;
+   echo $sum;
+?>
